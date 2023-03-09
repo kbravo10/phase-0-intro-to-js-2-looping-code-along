8fbfa15cd155b761fad6f5869419e276f2378043
Kevin Bravo <kbravo1024@gmail.com>
2023-03-09 06:00:35 -0800

    Complet Loop labe

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+const newArray = [];
+let message;
+
+function writeCards(arrayString, eventName){
+    for(let i = 0; i < arrayString.length; i++){
+        message = `Thank you, ${arrayString[i]}, for the wonderful ${eventName} gift!`
+        newArray[i] = message;
+        console.log
+    }
+    return newArray;
+}
+
+function countDown(num){
+    while(num >= 0){
+        console.log(num);
+        num--;
+    }
+}
